a5eefd6feca8a18ffc7ab45af8a9ce4d676d1c84
Remi-120101~ <remi.nkjm@gmail.com>
2022-11-21 11:22:37 +0900

    v-model

--- root/javascripts/validation.js
+++ root/javascripts/validation.js
@@ -2,5 +2,12 @@ const app = new Vue({
     el: '#app',
     data: {
       errorMessage: 'error!',
+      // 以下を追加
+      formData: {
+        name: '',
+        email: '',
+        email_2: '',
+        content: '',
+      },
     }
   })
