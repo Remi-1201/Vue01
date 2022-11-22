b20f9b710a6186e13a62f9e2397d0c40b1ab3dd3
Remi-120101~ <remi.nkjm@gmail.com>
2022-11-22 11:11:10 +0900

    v-if

--- root/javascripts/validation.js
+++ root/javascripts/validation.js
@@ -1,12 +1,12 @@
 const app = new Vue({
     el: '#app',
     data: {
-      errorMessage: 'error!',
+      errorMessage: 'Eメールが一致しません',
       // v-model- 以下を追加
       formData: {
         name: '',
         email: '',
-        email_2: '',
+        emailConfirm: '',
         content: '',
       },
     }
